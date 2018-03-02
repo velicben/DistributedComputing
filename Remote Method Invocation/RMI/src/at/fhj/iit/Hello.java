06e1c761d9d4850a80220559b65f07f859760034
Michael Ulm <michael.ulm@fh-joanneum.at>
2018-03-02 16:13:27 +0100

    + RMI Example for getting started

---
+++ Remote Method Invocation/RMI/src/at/fhj/iit/Hello.java
@@ -0,0 +1,11 @@
+package at.fhj.iit;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * simple remote interface for using RMI remote object
+ */
+public interface Hello extends Remote {
+    String sayHello() throws RemoteException;
+}
